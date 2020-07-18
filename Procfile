0e0fdb8d88140123a79133a3f85a23d3caba8072
Edudeiko <evgeniy.dudeyko@gmail.com>
2020-07-17 21:23:17 -0400

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn "web_app:create_app()"
+web: gunicorn sp_search_track_app:server
--- web_app/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Entry point for our twitter app flask app."""
-
-from .sp_search_track_app import create_app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
